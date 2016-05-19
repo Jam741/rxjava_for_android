2d282ffaff3f4180370948a9d5de1f76371bfa7d
jam <991654396@qq.com>
2016-05-19 11:43:14 +0800

    retrofit test begin

---
+++ app/src/main/java/com/che58/ljb/rxjava/api/MoveInfoApi.java
@@ -0,0 +1,8 @@
+package com.che58.ljb.rxjava.api;
+
+/**
+ * Created by Administrator on 2016/5/19.
+ */
+public class MoveInfoApi {
+
+}
---
+++ app/src/main/java/com/che58/ljb/rxjava/api/server/RApiService.java
@@ -0,0 +1,7 @@
+package com.che58.ljb.rxjava.api.server;
+
+/**
+ * Created by Administrator on 2016/5/19.
+ */
+public interface RApiService {
+}
